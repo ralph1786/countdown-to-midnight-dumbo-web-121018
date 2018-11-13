2bd041bb4af7c6eda5e0f8fa1c134020179f6ad5
Rafael Cruz <ralph1786@gmail.com>
2018-11-13 21:02:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-# x = 10
-def countdown(int)
-  while int < 10
-  int -= 1 
-  puts "#{int} SECONDS!"
+
+def countdown(10)
+  while x < 10
+  x -= 1 
+  puts "#{x} SECONDS!"
 end
 end
